0adf53d51cf01d191dcbc53bc63c11d383b812dc
sooraj8136 <soorajcpchathanathparampil@gmail.com>
2026-06-11 22:06:26 +0530

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,21 @@ import ServicesPage from "./pages/ServicesPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import ScrollToTop from "./components/ScrollToTop";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
       <ScrollToTop />
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
-
+      <Footer />
       <Toaster theme="dark" position="bottom-right" />
     </>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,23 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+
 import Hero from "../sections/Hero";
-import Work from "../sections/Work";
 import Services from "../sections/Services";
 import About from "../sections/About";
-// import Team from "../sections/Team";
 import Technologies from "../sections/Technologies";
-import Pricing from "../sections/Pricing";
 import Contact from "../sections/Contact";
 
 export default function Home() {
   return (
     <div className="bg-black text-white">
-      <Navbar />
       <main>
         <Hero />
         {/* <Work /> */}
-        <About />   
+        <About />
         <Services />
         {/* <Team /> */}
         <Technologies />
         {/* <Pricing /> */}
         <Contact />
       </main>
-      <Footer />
     </div>
   );
 }
